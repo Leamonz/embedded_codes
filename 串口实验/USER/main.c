6d1486cdbdf53eae384b7cdd40440289915b25b4
Leamonz <3262305069@qq.com>
2022-08-18 10:42:13 +0800

    problems to be solved dat:08_18

--- 串口实验/USER/main.c
+++ 串口实验/USER/main.c
@@ -1,31 +1,57 @@
 #include "stm32f10x.h"
+#include "usart.h"
+#include <stdio.h>
 
- void Delay(u32 count)
- {
-   u32 i=0;
-   for(;i<count;i++);
- }
- int main(void)
- {	
-  GPIO_InitTypeDef  GPIO_InitStructure;
-	 
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|
-  RCC_APB2Periph_GPIOE, ENABLE);	    //使能PB,PE端口时钟
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;			    //LED0-->PB.5 端口配置
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 	 //推挽输出
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	 //IO口速度为50MHz
-  GPIO_Init(GPIOB, &GPIO_InitStructure);			     //初始化GPIOB.5
-  GPIO_SetBits(GPIOB,GPIO_Pin_5);					//PB.5 输出高
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;	            //LED1-->PE.5推挽输出
-  GPIO_Init(GPIOE, &GPIO_InitStructure);	  	       //初始化GPIO
-  GPIO_SetBits(GPIOE,GPIO_Pin_5); 			 //PE.5 输出高 	  
-  while(1)
-	{
-	    GPIO_ResetBits(GPIOB,GPIO_Pin_5);
-	    GPIO_SetBits(GPIOE,GPIO_Pin_5);
-		Delay(3000000);
-		GPIO_SetBits(GPIOB,GPIO_Pin_5);
-		GPIO_ResetBits(GPIOE,GPIO_Pin_5);
-		Delay(3000000);
+
+void USART1_Init(void){
+	GPIO_InitTypeDef GPIO_InitStruct;
+	USART_InitTypeDef USART_InitStruct;
+	NVIC_InitTypeDef NVIC_InitStruct;
+	//①使能相关时钟
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_USART1, ENABLE);
+	//②初始化IO口
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;//模式查表
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	//③初始化串口相关参数
+	USART_InitStruct.USART_BaudRate = 115200;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//不使用硬件流	
+	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;//发送和接收使能
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	
+	USART_Init(USART1, &USART_InitStruct);
+	//④使能串口1
+	USART_Cmd(USART1, ENABLE);
+	//⑤开启中断
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//设置发送中断
+	//⑤进行中断优先级的设置
+	NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 1;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStruct);
+}
+//⑥中断服务函数
+void USART1_IRQHandler(void){
+	//接收数据的变量
+	u8 res;
+	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET){
+		res = USART_ReceiveData(USART1);
+		USART_SendData(USART1, res);
 	}
- }
+}
+
+int main(void)
+{	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为2
+	USART1_Init();
+	while(1);
+}
+
---
+++ 串口实验/USER/usart.h
@@ -0,0 +1,8 @@
+#ifndef __USART_H__
+#define __USART_H__
+
+void USART1_Init(void);
+
+
+#endif
+
